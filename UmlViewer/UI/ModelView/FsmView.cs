2539576ddf12e811f273cd088002915852328240
RadicalRumin <34235017+RadicalRumin@users.noreply.github.com>
2025-08-21 15:12:23 +0200

    Make initial and final states required

--- UmlViewer/UI/ModelView/FsmView.cs
+++ UmlViewer/UI/ModelView/FsmView.cs
@@ -3,8 +3,8 @@ namespace UmlViewer.UI.ModelView;
 public sealed class FsmView
 {
     public string Title { get; init; } = "FSM";
-    public StateView? Initial { get; init; }
-    public StateView? Final { get; init; }
+    public required StateView Initial { get; init; }
+    public required StateView Final { get; init; }
     public List<StateView> RootStates { get; } = new();
     public List<TransitionView> TopLevelTransitions { get; } = new();
 }
