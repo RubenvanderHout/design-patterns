7b32b844a5d97aef356bfaaf48f84875916f9487
RadicalRumin <34235017+RadicalRumin@users.noreply.github.com>
2025-08-24 13:37:48 +0200

    removed old function

--- Validation/ValidationRules/ValidationRuleBuilder.cs
+++ Validation/ValidationRules/ValidationRuleBuilder.cs
@@ -8,23 +8,6 @@ namespace Validation.ValidationRules
 {
     public static class ValidationRuleBuilder
     {
-        // public static List<IValidationRule> GiveAllValidationRules()
-        // {
-        //     var list = new List<IValidationRule>()
-        //     {
-        //         new CompoundStateRule(),
-        //         new FinalStateRule(),
-        //         new IdentifierRule(),
-        //         new InitialStateRule(),
-        //         new NonDeterministicTransitionsRule(),
-        //         new TransitionRule(),
-        //         new TransitionToCompoundStateRule(),
-        //         new UnreachableStateRule(),
-        //     };
-
-        //     return list;
-        // }
-
         public static IRuleComponent BuildCompleteComposite()
         {
             var stateRules = new RuleGroup("State rules")
